df57defcb529ec4fa38b7a828fe335102f213ae1
Dan Spencer <da_spencer@hotmail.com>
2016-07-04 23:54:18 -0400

    Added additional tests to the vendor search.

--- test/vendor/vendor_test.rb
+++ test/vendor/vendor_test.rb
@@ -9,6 +9,11 @@ class CveSearchVendorTest < Minitest::Test
     VCR.use_cassette('one_vendor') do
       vendor = CveSearch::Vendor.find('Microsoft')
       assert_equal CveSearch::Vendor, vendor.class
+
+      assert_equal "microsoft", vendor.name
+      assert_equal Array, vendor.products.class
+      assert_equal 433, vendor.products.length
+      assert_equal ".net_framework", vendor.products[0]
     end
   end
 end
