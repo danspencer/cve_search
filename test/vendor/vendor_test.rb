142dddad957b0866e489b054ec7a04923c428106
Dan Spencer <da_spencer@hotmail.com>
2016-07-06 22:31:25 -0400

    Minor refactoring of vendor tests.

--- test/vendor/vendor_test.rb
+++ test/vendor/vendor_test.rb
@@ -12,16 +12,17 @@ class CveSearchVendorTest < Minitest::Test
       assert_equal "microsoft", vendor.name
       assert_equal Array, vendor.products.class
       assert_equal 433, vendor.products.length
-      assert_equal ".net_framework", vendor.products[0]
+      assert_equal ".net_framework", vendor.products.first
     end
   end
 
   def test_it_gives_back_all_vendors
     VCR.use_cassette('all_vendors') do
       vendors = CveSearch::Vendor.all
-      assert_equal 5000, vendors.length
-      assert_equal CveSearch::Vendor, vendors[0].class
-      assert_equal "%240.99_kindle_books_project", vendors[0].name
+      assert vendors.kind_of? Array
+      assert_equal vendors.length, 5000
+      assert vendors.first.kind_of? CveSearch::Vendor
+      assert_equal vendors.first.name, "%240.99_kindle_books_project"
     end
   end
 end
